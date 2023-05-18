fdd3b2dc25eff59ba702ffaf7f4270703d513807
DmitriyNZ-Barashenkov <133701607+DmitriyNZ-Barashenkov@users.noreply.github.com>
2023-05-19 02:06:29 +0300

    rolled back the version to a working state, fixed syntax errors.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Основной файл запуска frequency_penalty=0.0,, presence_penalty=0.6, temperature=0.5, max_tokens=1024, top_p=1.0, stop=None
 
 import logging
-from communi_bot import os
+#from communi_bot import os
 
 from aiogram import types
 from aiogram.utils import executor
@@ -21,11 +21,11 @@ logging.info("Отладочная информация")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-async def on_startup(dp):
-    await bot.set_webhook(os.getenv("HOST_DOMEN"))
+#async def on_startup(dp):
+#    await bot.set_webhook(os.getenv("HOST_DOMEN"))
 
-async def on_shotdawn(dp):
-    await bot.delete_webhook()
+#async def on_shotdawn(dp):
+#    await bot.delete_webhook()
 
 
 
@@ -35,5 +35,5 @@ handler.register_handlers(dp)
 
 
 if __name__ == "__main__":
-    #executor.start_polling(dp, skip_updates=True)
-    executor.start_webhook(dispatcher=dp, webhook_path="", on_startup=on_startup, on_shutdown=on_shotdawn, skip_updates=True, host='0.0.0.0', port=int(os.environ.get('PORT',8443)))
+    executor.start_polling(dp, skip_updates=True)
+    #executor.start_webhook(dispatcher=dp, webhook_path="", on_startup=on_startup, on_shutdown=on_shotdawn, skip_updates=True, host='0.0.0.0', port=int(os.environ.get('PORT',3001)))
